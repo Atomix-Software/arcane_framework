edb6fce02e1ae4897c8f78ddab0824e972fe3d2a
ArcaneSunku <t4hnner@gmail.com>
2025-07-15 06:29:38 -0500

    Added a compiler flag that decides if there should be a file log

--- Arcane/src/Arcane/Core/Log.cpp
+++ Arcane/src/Arcane/Core/Log.cpp
@@ -15,10 +15,12 @@ namespace Arcane
 	{
 		std::vector<spdlog::sink_ptr> logSinks;
 		logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
-		logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("Arcane.log", true));
-
 		logSinks[0]->set_pattern("%^[%T] %n: %v%$");
+
+#ifdef ARC_LOG_FILE
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("Arcane.log", true));
 		logSinks[1]->set_pattern("%^[%T] [%l] %n: %v%$");
+#endif
 
 		s_CoreLogger = CreateShared<spdlog::logger>("ARCANE", begin(logSinks), end(logSinks));
 		s_CoreLogger->set_level(spdlog::level::trace);
